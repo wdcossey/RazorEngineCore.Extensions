610e7237db94ff2331141f0cfad48cb9e9d1e2bb
William David Cossey <wdcossey@gmail.com>
2020-06-07 15:32:28 +0100

    Finished up RazorEngineCoreJsonWriter.cs Cleaned up RazorEngineCoreHtmlWriter.cs Updated SampleApp.

--- RazorEngineCore.Extensions/RazorEngineCoreJsonWriter.cs
+++ RazorEngineCore.Extensions/RazorEngineCoreJsonWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Encodings.Web;
+﻿using System;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 // ReSharper disable MemberCanBePrivate.Global
@@ -6,48 +7,104 @@ namespace RazorEngineCore
 {
     /// <summary>
     /// Lightweight implementation of the IJsonHelper from ASP.Net Core.
-    /// The purpose of this file is to allow use of some @Json. tags in RazorEngineCore.
+    /// The purpose of this file is to allow use of @Json tags in RazorEngineCore.
     /// </summary>
     public class RazorEngineCoreJsonWriter
     {
-        private JsonSerializerOptions SerializerOptions { get; } = new JsonSerializerOptions()
+        internal const int BufferSizeDefault = 16 * 1024;
+        internal const int DefaultMaxDepth = 64;
+        
+        private JsonSerializerOptions GlobalSerializerOptions { get; } = new JsonSerializerOptions()
         {
             Encoder = JavaScriptEncoder.Default
         };
         
+        private static IHtmlContent Serialize(object value, JsonSerializerOptions options)
+        {
+            var json = JsonSerializer.Serialize(value, options);
+            return new HtmlString(json);
+        }
+
+        private static int SafeMaxDepth(int value)
+        {
+            return (value <= 0) ? DefaultMaxDepth : value;
+        }
+        
+        private static int SafeBufferSize(int value)
+        {
+            return (value <= 0) ? 1 : value;
+        }
+        
         public IHtmlContent Serialize(object value)
         {
-            var json = JsonSerializer.Serialize(value, SerializerOptions);
-            return new HtmlString(json);
+            return Serialize(value, GlobalSerializerOptions);
+        }
+        
+        // ReSharper disable once MethodOverloadWithOptionalParameter
+        public IHtmlContent Serialize(
+            object value, 
+            bool? allowTrailingCommas = null,
+            int? defaultBufferSize = null,
+            bool? ignoreNullValues = null,
+            bool? ignoreReadOnlyProperties = null,
+            int? maxDepth = null,
+            bool? propertyNameCaseInsensitive = null,
+            bool? writeIndented = null)
+        {
+            var serializerOptions = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Default,
+                DefaultBufferSize = SafeBufferSize(defaultBufferSize ?? GlobalSerializerOptions.DefaultBufferSize),
+                WriteIndented = writeIndented ?? GlobalSerializerOptions.WriteIndented,
+                AllowTrailingCommas = allowTrailingCommas ?? GlobalSerializerOptions.AllowTrailingCommas,
+                IgnoreNullValues = ignoreNullValues ?? GlobalSerializerOptions.IgnoreNullValues,
+                MaxDepth =  SafeMaxDepth(maxDepth ?? GlobalSerializerOptions.MaxDepth),
+                IgnoreReadOnlyProperties = ignoreReadOnlyProperties ?? GlobalSerializerOptions.IgnoreReadOnlyProperties,
+                PropertyNameCaseInsensitive = propertyNameCaseInsensitive ?? GlobalSerializerOptions.PropertyNameCaseInsensitive,
+            };
+            
+            return Serialize(value, serializerOptions);
         }
 
         public RazorEngineCoreJsonWriter WriteIndented(bool value)
         {
-            SerializerOptions.WriteIndented = value;
+            GlobalSerializerOptions.WriteIndented = value;
             return this;
         }
 
         public RazorEngineCoreJsonWriter IgnoreNullValues(bool value)
         {
-            SerializerOptions.IgnoreNullValues = value;
+            GlobalSerializerOptions.IgnoreNullValues = value;
             return this;
         }
 
         public RazorEngineCoreJsonWriter MaxDepth(int value)
         {
-            SerializerOptions.MaxDepth = value;
+            GlobalSerializerOptions.MaxDepth = SafeMaxDepth(value);
             return this;
         }
 
         public RazorEngineCoreJsonWriter AllowTrailingCommas(bool value)
         {
-            SerializerOptions.AllowTrailingCommas = value;
+            GlobalSerializerOptions.AllowTrailingCommas = value;
             return this;
         }
 
         public RazorEngineCoreJsonWriter IgnoreReadOnlyProperties(bool value)
         {
-            SerializerOptions.IgnoreReadOnlyProperties = value;
+            GlobalSerializerOptions.IgnoreReadOnlyProperties = value;
+            return this;
+        }
+        
+        public RazorEngineCoreJsonWriter PropertyNameCaseInsensitive(bool value)
+        {
+            GlobalSerializerOptions.PropertyNameCaseInsensitive = value;
+            return this;
+        }
+        
+        public RazorEngineCoreJsonWriter DefaultBufferSize(int value)
+        {
+            GlobalSerializerOptions.DefaultBufferSize = SafeBufferSize(value);
             return this;
         }
     }
